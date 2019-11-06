8d45045cc076d4285c01950762de6ba86fa0282c
wabomba-bakar <wabombabakar@gmail.com>
2019-11-06 15:29:34 +0300

    add a build fold

---
+++ build/precache-manifest.bc441ef8178eb7a3d1dbec3752154c3d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ded86cd2b7b42a00b3ce875ad2e1e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dafd7d085a85758e8f31",
+    "url": "/static/css/main.cf4fa88d.chunk.css"
+  },
+  {
+    "revision": "996ded0e6a906786b758",
+    "url": "/static/js/2.9697cc01.chunk.js"
+  },
+  {
+    "revision": "dafd7d085a85758e8f31",
+    "url": "/static/js/main.bb5a2b1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "453dfad76ade326a966704b67077bc99",
+    "url": "/static/media/defaultBcg3.453dfad7.jpg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/static/media/room-9.7b17c49f.jpeg"
+  },
+  {
+    "revision": "d88b2d1bddd4c9a342d682af4b179aa5",
+    "url": "/static/media/room-hero.d88b2d1b.jpg"
+  }
+]);
