e0eb80e9d295d1809f62115a543c859e35d6e2f2
wabomba-bakar <wabombabakar@gmail.com>
2019-11-06 14:57:05 +0300

    modified the css

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
-import {FaAlignRight} from 'react-icons/fa';
-import Logo from '../../images/logo.svg';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { FaAlignRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   state = {
@@ -9,24 +8,32 @@ class Navbar extends Component {
   };
 
   onChange = () => {
-    this.setState({ isOpen: !this.state.isOpen })
+    this.setState({ isOpen: !this.state.isOpen });
   };
   render() {
     return (
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <Link to="/"><img src={Logo} alt="logo"/></Link>
-            <button type="button" className="nav-btn" onClick={this.onChange}>
-              <FaAlignRight className="nav-icon"/>
+      <nav className='navbar'>
+        <div className='nav-center'>
+          <div className='nav-header'>
+            <Link to='/'>
+              <div className='logo'>
+                <h3>
+                  <span>Resort</span> Hotel
+                </h3>
+              </div>
+            </Link>
+            <button type='button' className='nav-btn' onClick={this.onChange}>
+              <FaAlignRight className='nav-icon' />
             </button>
           </div>
-          <ul className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links' }>
+          <ul
+            className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
+          >
             <li>
-              <Link to="/">Home</Link>
+              <Link to='/'>Home</Link>
             </li>
             <li>
-              <Link to="/rooms">Rooms</Link>
+              <Link to='/rooms'>Rooms</Link>
             </li>
           </ul>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,16 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import FeaturedRooms from "../components/FeaturedRooms";
-import Hero from "../components/Hero";
-import Banner from "../components/Banner";
-import Services from "../components/Services";
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import FeaturedRooms from '../components/FeaturedRooms';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 const Home = () => {
   return (
     <Fragment>
-      <Hero hero="defaultHero">
-        <Banner
-          title="Luxurious Rooms"
-          subtitle="deluxe rooms starting at $299"
-        >
-          <Link to="/rooms" className="btn-primary">
+      <Hero hero='defaultHero'>
+        <Banner title='BOOK NOW!' subtitle='deluxe rooms starting at $299'>
+          <Link to='/rooms' className='btn-primary'>
             Our Rooms
           </Link>
         </Banner>
